d0f76e90336c4198a82f6fe4e89f41ce05bc2cbb
Heather Nuffer <zzzuzz@gmail.com>
2019-11-04 16:16:05 -0700

    Stuck TopRow of scoreboard into its own component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import "./App.css";
 import Header from "./Header";
+import TopRow from "./TopRow";
 import BottomRow from "./BottomRow";
 import ButtonBox from "./ButtonBox";
 
@@ -22,21 +23,8 @@ function App() {
     <div className="container">
       <Header />
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lord Licorice</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded 
-            values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Queen Frostine</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
+        <TopRow homeScore={homeScore}
+          awayScore={awayScore}/>
         <BottomRow downNum={downNum}
           toGoNum={toGoNum} 
           ballOnNum = {ballOnNum} 
